4f6fc6689c0e7647da61d6466434e47663a3c691
jseara118 <jchavarria670331@ufide.ac.cr>
2025-04-09 18:54:10 -0600

    Add project files.

---
+++ PetCareSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetCareSystem.Startup))]
+namespace PetCareSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
